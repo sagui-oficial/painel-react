ee7901aa11f3092d7a6e2f54d519c071627fbe89
Bruno Macedo <contato@brunomacedo.com.br>
2019-01-27 18:04:36 -0200

    [PAINEL] feat: create redux store

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,43 @@
-/* global document */
+/* global document window */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
+/* LOG REDUX STORE ON CONSOLE */
+// import logger from 'redux-logger';
+
+import reducers from './reducers';
 import './assets/styles/default.sass';
 import * as serviceWorker from './serviceWorker';
 import DashboardLayout from './layouts/Dashboard.layout';
 
 const hist = createBrowserHistory();
 
+/**
+ * Chrome Extension Redux Store
+ * @param __REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+ */
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-enable */
+
+const store = createStore(
+  reducers,
+  composeEnhancers(
+    applyMiddleware(thunk),
+  ),
+);
+
 ReactDOM.render(
-  <Router history={hist}>
-    <DashboardLayout />
-  </Router>,
+  <Provider store={store}>
+    <Router history={hist}>
+      <DashboardLayout />
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
 
