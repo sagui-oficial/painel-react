8c7dfa89d144ae95b169f10406bb5d109b373253
Bruno Macedo <contato@brunomacedo.com.br>
2019-01-27 18:03:03 -0200

    [PAINEL] feat: create helpers formatCurrency, formatDate, RandomPrice

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,1 +1,26 @@
-// SET
+export function getUNIXDate() {
+  const getDate = new Date();
+  const day = `0${getDate.getDate()}`;
+  const month = `0${getDate.getMonth() + 1}`;
+  const year = getDate.getFullYear();
+
+  return `${day.substr(-2)}/${month.substr(-2)}/${year}`;
+}
+
+export const formatCurrency = (_number, _locale = 'pt-BR', _format = 'BRL') => new Intl.NumberFormat(_locale, {
+  style: 'currency',
+  currency: _format,
+  minimumFractionDigits: 2,
+}).format(_number);
+
+export function randomPrice(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+export function randomStatusGuias() {
+  const statusItems = ['Vencida', 'Paga', 'Enviada', 'Cancelada', 'Glosada'];
+  const randomItem = Math.floor(Math.random() * statusItems.length);
+  return statusItems[randomItem];
+}
+
+export default { getUNIXDate, formatCurrency };
