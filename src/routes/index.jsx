7f2b68d388bcbe9411ac39b703f45187d9244f62
Bruno Macedo <contato@brunomacedo.com.br>
2019-01-30 21:36:27 -0200

    [PAINEL] feat: set root router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -19,7 +19,7 @@ const MainRouters = () => (
     ))}
     <Route exact path="/guias/:id" component={FormGuia} />
     <Route exact path="/login" component={Login} />
-    <Route path="/" render={() => (<Redirect to="/dashboard" />)} />
+    <Route path="/" render={() => (<Redirect to="/guias" />)} />
     {/* <Route component={NotFoundPage} /> */}
   </Switch>
 );
