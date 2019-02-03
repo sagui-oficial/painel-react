d2d4e7adef63e9996fa50f92ac05fea7278c4aae
Bruno Macedo <contato@brunomacedo.com.br>
2019-02-03 18:49:36 -0200

    feat: fetch error from api

--- src/actions/guias.js
+++ src/actions/guias.js
@@ -3,38 +3,39 @@ import { APIResquest } from '../utils/API';
 export const GET_GUIAS = 'GET_GUIAS';
 export const SAVE_GUIAS = 'SAVE_GUIAS';
 export const DELETE_GUIAS = 'DELETE_GUIAS';
+export const FETCH_ERROR = 'FETCH_ERROR';
 
 export function loadGuias() {
-  return (dispatch) => {
-    APIResquest({
+  return async (dispatch) => {
+    await APIResquest({
       uri: 'guias',
       method: 'GET',
-    }).then((res) => {
-      dispatch({ type: GET_GUIAS, payload: res });
-    });
+    })
+      .then(data => dispatch({ type: GET_GUIAS, payload: data }))
+      .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
   };
 }
 
 export function addGuia(data) {
-  return (dispatch) => {
-    APIResquest({
+  return async (dispatch) => {
+    await APIResquest({
       uri: 'guias',
       method: 'POST',
       data,
-    }).then((res) => {
-      dispatch({ type: SAVE_GUIAS, payload: res });
-    });
+    })
+      .then(res => dispatch({ type: SAVE_GUIAS, payload: res }))
+      .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
   };
 }
 
 export function deleteGuias(id) {
-  return (dispatch) => {
-    APIResquest({
+  return async (dispatch) => {
+    await APIResquest({
       uri: `guias/${id}`,
       method: 'DELETE',
       data: id,
-    }).then(() => {
-      dispatch({ type: DELETE_GUIAS, payload: id });
-    });
+    })
+      .then(() => dispatch({ type: DELETE_GUIAS, payload: id }))
+      .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
   };
 }
