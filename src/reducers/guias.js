1f35409ec3c588bbf3de21b48210f5ab01c917ce
Bruno Macedo <contato@brunomacedo.com.br>
2019-02-03 18:49:57 -0200

    feat: create fetch error

--- src/reducers/guias.js
+++ src/reducers/guias.js
@@ -1,7 +1,10 @@
-import { GET_GUIAS, SAVE_GUIAS, DELETE_GUIAS } from '../actions/guias';
+import {
+  GET_GUIAS, SAVE_GUIAS, DELETE_GUIAS, FETCH_ERROR,
+} from '../actions/guias';
 
 const INITIAL_STATE = {
   guias: [],
+  fetchError: null,
 };
 
 export default function (state = INITIAL_STATE, action = {}) {
@@ -10,16 +13,25 @@ export default function (state = INITIAL_STATE, action = {}) {
       return {
         ...state,
         guias: action.payload,
+        fetchError: null,
       };
     case SAVE_GUIAS:
+      console.warn(action.payload);
       return {
         ...state,
         guias: state.guias.concat([action.payload]),
+        fetchError: null,
       };
     case DELETE_GUIAS:
       return {
         ...state,
         guias: state.guias.filter(guia => guia.id !== action.payload),
+        fetchError: null,
+      };
+    case FETCH_ERROR:
+      return {
+        ...state,
+        fetchError: action.payload,
       };
     default:
       return state;
