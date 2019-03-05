9ca80213226c8e7867c8e58780b38460b78a485a
Bruno Macedo <contato@brunomacedo.com.br>
2019-03-05 18:33:23 -0300

    feat: create action guia detail

--- src/actions/guias.js
+++ src/actions/guias.js
@@ -1,8 +1,8 @@
 // import io from 'socket.io-client';
-
 import { APIResquest } from '../utils/API';
 
 export const GET_GUIAS = 'GET_GUIAS';
+export const GET_GUIAS_DETAILS = 'GET_GUIAS_DETAILS';
 export const SAVE_GUIAS = 'SAVE_GUIAS';
 export const DELETE_GUIAS = 'DELETE_GUIAS';
 export const FETCH_ERROR = 'FETCH_ERROR';
@@ -12,42 +12,35 @@ export function loadGuias() {
   return async (dispatch) => {
     await APIResquest({
       uri: 'guias',
-      // uri: 'listargto',
       method: 'GET',
     })
       .then(data => dispatch({ type: GET_GUIAS, payload: data }))
-      // .then(data => dispatch({ type: GET_GUIAS, payload: data.Result.GTOs }))
       .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
   };
 }
 
 export function addGuia(data) {
-  // console.log(JSON.stringify(data));
   return async (dispatch) => {
     await APIResquest({
       uri: 'guias',
-      // uri: 'criargto',
       method: 'POST',
       data,
     })
       .then(res => dispatch({ type: SAVE_GUIAS, payload: res }))
-      // .then(res => dispatch({ type: SAVE_GUIAS, payload: res.Result.GTO }))
       .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
   };
 }
 
-/* export function deleteGuias(data) {
+export function loadGuiaDetail(id) {
   return async (dispatch) => {
     await APIResquest({
-      // uri: `${id}/deletargto`,
-      uri: 'atualizargto',
-      method: 'PUT',
-      data,
+      uri: `guias/${id}`,
+      method: 'GET',
     })
-      .then(res => dispatch({ type: DELETE_GUIAS, payload: res }))
+      .then(data => dispatch({ type: GET_GUIAS_DETAILS, payload: data }))
       .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
   };
-} */
+}
 
 export function deleteGuias(data, id) {
   return async (dispatch) => {
@@ -60,18 +53,6 @@ export function deleteGuias(data, id) {
       .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
   };
 }
-
-/* export function deleteGuias(id) {
-  return async (dispatch) => {
-    await APIResquest({
-      uri: `guias/${id}`,
-      method: 'DELETE',
-      data: id,
-    })
-      .then(() => dispatch({ type: DELETE_GUIAS, payload: id }))
-      .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
-  };
-} */
 
 /* export function connectIO() {
   return (dispatch) => {
