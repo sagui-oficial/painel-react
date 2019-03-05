6978f4279498b2b99919dcd030da2b4736197596
Bruno Macedo <contato@brunomacedo.com.br>
2019-03-05 16:23:38 -0300

    spec: rebuild tests

--- src/__tests__/layouts/Dashboard.test.jsx
+++ src/__tests__/layouts/Dashboard.test.jsx
@@ -3,17 +3,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
+import reducers from '../../reducers';
 import DashboardLayout from '../../layouts/Dashboard';
 
 const hist = createBrowserHistory();
 
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk),
+);
+
 it('Renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <Router history={hist}>
-      <DashboardLayout />
-    </Router>,
+    <Provider store={store}>
+      <Router history={hist}>
+        <DashboardLayout />
+      </Router>
+    </Provider>,
     div,
   );
   ReactDOM.unmountComponentAtNode(div);
