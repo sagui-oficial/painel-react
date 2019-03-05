810f2076a950be520fcd85f62e89fa107abb2656
Bruno Macedo <contato@brunomacedo.com.br>
2019-03-05 03:32:44 -0300

    feat: create custom theme styles

--- src/withRoot.jsx
+++ src/withRoot.jsx
@@ -1,20 +1,62 @@
 import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { purple } from '@material-ui/core/colors';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 const theme = createMuiTheme({
   typography: {
     useNextVariants: true,
   },
+  roundedBtn: {
+    borderRadius: '50rem',
+    borderColor: '#e4e4e4',
+    textTransform: 'inherit',
+  },
+  divider: {
+    position: 'relative',
+    margin: '1rem auto',
+    overflow: 'inherit',
+    '&:before': {
+      content: "''",
+      left: 0,
+      top: '-0.15rem',
+      position: 'absolute',
+      background: '#5b02c1',
+      width: '1.5rem',
+      height: '0.3rem',
+    },
+  },
   palette: {
-    primary: {
-      light: purple[300],
-      main: purple[500],
-      dark: purple[700],
+    common: {
+      black: '#000',
+      white: '#fff',
     },
     background: {
-      default: '#ffffff',
+      paper: '#fff',
+      default: '#fff',
+    },
+    primary: {
+      light: '#7986cb',
+      main: '#5b02c1',
+      dark: '#303f9f',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff4081',
+      main: '#f50057',
+      dark: '#c51162',
+      contrastText: '#fff',
+    },
+    error: {
+      light: '#e57373',
+      main: '#f44336',
+      dark: '#d32f2f',
+      contrastText: '#fff',
+    },
+    text: {
+      primary: 'rgba(0, 0, 0, 0.87)',
+      secondary: 'rgba(0, 0, 0, 0.54)',
+      disabled: 'rgba(0, 0, 0, 0.38)',
+      hint: 'rgba(0, 0, 0, 0.38)',
     },
   },
 });
