1b4250dadf8404bd153ca6b6d65827f6874f3b3d
Bruno Macedo <contato@brunomacedo.com.br>
2019-03-06 10:45:46 -0300

    [FEAT] set new route create guia

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import dashboardRoutes from './dashbordRoutes';
 import Login from '../views/Login';
 import GuiaDetail from '../views/Guias/Detail';
+import GuiaCreate from '../views/Guias/Create';
 
 const MainRouters = () => (
   <Switch>
@@ -17,8 +18,9 @@ const MainRouters = () => (
         />
       )
     ))}
-    <Route exact path="/guias/:id" component={GuiaDetail} />
     <Route exact path="/login" component={Login} />
+    <Route exact path="/guias/criar" component={GuiaCreate} />
+    <Route exact path="/guias/:id" component={GuiaDetail} />
     <Route path="/" render={() => (<Redirect to="/guias" />)} />
   </Switch>
 );
