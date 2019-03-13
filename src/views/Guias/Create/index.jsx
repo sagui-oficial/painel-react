500d7d2f97b23d1fffc1f231a36fadab3253f929
Bruno Macedo <contato@brunomacedo.com.br>
2019-03-12 23:05:32 -0300

    [FEAT] form create

--- src/views/Guias/Create/index.jsx
+++ src/views/Guias/Create/index.jsx
@@ -1,21 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import uuidv1 from 'uuid/v1';
 
 import { withStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
+import { Breadcrumbs } from '@material-ui/lab';
+import {
+  Button,
+  Typography,
+  Grid,
+  Divider,
+  Chip,
+  Avatar,
+  TextField,
+} from '@material-ui/core';
+
+import { Home as HomeIcon } from '@material-ui/icons';
 
 import { addGuia } from '../../../actions/guias';
 import { randomPrice, randomNames } from '../../../helpers';
 
 const styles = theme => ({
+  chip: {
+    backgroundColor: theme.palette.grey[100],
+    height: 24,
+    cursor: 'pointer',
+    color: theme.palette.grey[800],
+    fontWeight: theme.typography.fontWeightRegular,
+    '&:hover, &:focus, &.active': {
+      backgroundColor: theme.palette.grey[300],
+    },
+    '&:active': {
+      boxShadow: theme.shadows[1],
+    },
+  },
+  activeChip: {
+    backgroundColor: theme.palette.grey[300],
+  },
+  avatar: {
+    background: 'none',
+    marginRight: -theme.spacing.unit * 1.5,
+  },
   box: {
     padding: '20px',
     border: '1px solid #ccc',
   },
+  divider: {
+    ...theme.divider,
+    marginBottom: theme.spacing.unit * 1.2,
+  },
   addBtn: {
     ...theme.roundedBtn,
+    marginLeft: '1.5rem',
+    '&.footerBtn': {
+      marginLeft: 0,
+      marginTop: theme.spacing.unit * 5,
+    },
     [theme.breakpoints.down('xs')]: {
       marginLeft: '0',
       marginTop: '0.5rem',
@@ -92,18 +133,120 @@ class GuiaCreate extends Component {
     const { classes } = this.props;
 
     return (
-      <div>
-        <Button
-          variant="outlined"
-          color="primary"
-          size="medium"
-          className={classes.addBtn}
-          onClick={this.handleAddGuia}
-        >
-          Salvar
-        </Button>
-        <h1 className={classes.box}>Guias</h1>
-      </div>
+      <Fragment>
+        <Grid container alignItems="center">
+          <Typography variant="h6" color="inherit" noWrap>
+            Cadastrar nova guia
+          </Typography>
+          <Button
+            variant="outlined"
+            color="primary"
+            size="medium"
+            className={classes.addBtn}
+            onClick={this.handleAddGuia}
+          >
+            Salvar
+          </Button>
+        </Grid>
+
+        <Divider className={classes.divider} />
+
+        <Breadcrumbs arial-label="Breadcrumb">
+          <Chip className={classes.chip} to="/guias" component={RouterLink} label="Guias" avatar={(<Avatar className={classes.avatar}><HomeIcon /></Avatar>)} />
+          <Chip to="/guias/criar" component={RouterLink} className={`${classes.chip} ${classes.activeChip}`} label="Cadastrar" />
+        </Breadcrumbs>
+
+        <form noValidate autoComplete="off">
+          <Grid container spacing={16}>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                fullWidth
+                label="Número da guia"
+                name="numero"
+                helperText="Digite o número da guia."
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                fullWidth
+                label="Status"
+                name="status"
+                margin="normal"
+              />
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={16}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Data de vencimento"
+                name="vencimento"
+                helperText="Ex.: 23/02/2019"
+                margin="normal"
+              />
+            </Grid>
+          </Grid>
+
+          <br />
+
+          <Grid container spacing={16}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Nome do paciente"
+                name="paciente"
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="CPF"
+                name="cpf"
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                fullWidth
+                label="Plano"
+                name="plano"
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                fullWidth
+                label="Telefone"
+                name="telefone"
+                margin="normal"
+                helperText="(11) 9000-0000"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="E-mail"
+                name="email"
+                margin="normal"
+                helperText="email@email.com.br"
+              />
+            </Grid>
+          </Grid>
+
+          <Button
+            variant="outlined"
+            color="primary"
+            size="medium"
+            className={`${classes.addBtn} footerBtn`}
+            onClick={this.handleAddGuia}
+          >
+            Salvar
+          </Button>
+        </form>
+      </Fragment>
     );
   }
 }
