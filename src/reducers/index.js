b023d149a70098d885c8b918929ffab91c6925f9
Bruno Macedo <contato@brunomacedo.com.br>
2019-03-18 01:24:50 -0300

    [FEAT] create patients components

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import guiasReducer from './guias';
+import pacientesReducer from './pacientes';
 import searchReducer from './search';
 
 export default combineReducers({
   guiasReducer,
+  pacientesReducer,
   searchReducer,
 });
