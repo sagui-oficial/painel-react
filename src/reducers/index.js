4d1862f65a77bb361f334bda2b7030658eed9bb1
Bruno Macedo <contato@brunomacedo.com.br>
2019-03-19 11:04:14 -0300

    feat: combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import guiasReducer from './guias';
 import pacientesReducer from './pacientes';
+import procedimentosReducer from './procedimentos';
 import searchReducer from './search';
 
 export default combineReducers({
   guiasReducer,
   pacientesReducer,
+  procedimentosReducer,
   searchReducer,
 });
