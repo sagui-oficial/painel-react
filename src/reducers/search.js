8200e2748e4da80ac0f5e3cebd4faf60a211bf24
Bruno Macedo <contato@brunomacedo.com.br>
2019-03-19 09:42:42 -0300

    Feat: reset search state reducer

--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,13 +1,15 @@
-import { SEARCH } from '../actions/search';
+import { SEARCH, RESET_SEARCH } from '../actions/search';
 
 const INITIAL_STATE = {
   inputValue: '',
 };
 
-export default function (state = INITIAL_STATE, action) {
-  switch (action.type) {
+export default function (state = INITIAL_STATE, { type, payload }) {
+  switch (type) {
     case SEARCH:
-      return { inputValue: action.payload };
+      return { inputValue: payload };
+    case RESET_SEARCH:
+      return state.inputValue;
     default:
       return state;
   }
