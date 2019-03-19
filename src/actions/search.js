ac71ed21ed14f20d3711f767a3118a1d69cad7f8
Bruno Macedo <contato@brunomacedo.com.br>
2019-03-19 09:42:21 -0300

    Feat: reset search state

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,8 +1,16 @@
 export const SEARCH = 'SEARCH';
+export const RESET_SEARCH = 'RESET_SEARCH';
 
 export function searchChange(e) {
   return {
     type: SEARCH,
     payload: e.target.value,
+  };
+}
+
+export function resetSearch() {
+  return {
+    type: SEARCH,
+    payload: '',
   };
 }
