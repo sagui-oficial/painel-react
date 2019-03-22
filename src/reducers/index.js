497076d8f87c225cae327f5b1bafc665d5374626
Bruno Macedo <contato@brunomacedo.com.br>
2019-03-22 17:08:35 -0300

    [feat] create reducer planos

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import guiasReducer from './guias';
 import pacientesReducer from './pacientes';
 import procedimentosReducer from './procedimentos';
+import planosReducer from './planos';
 
 export default combineReducers({
   guiasReducer,
   pacientesReducer,
   procedimentosReducer,
+  planosReducer,
 });
