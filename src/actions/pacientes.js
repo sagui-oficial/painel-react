19ce1d0ea13dfa7b10489141f88ef864837dcdeb
Bruno Macedo <contato@brunomacedo.com.br>
2019-03-22 20:49:39 -0300

    [feat] add patientes request

--- src/actions/pacientes.js
+++ src/actions/pacientes.js
@@ -1,6 +1,10 @@
 import { APIResquest } from '../utils/api';
 
 export const GET_PACIENTES = 'GET_PACIENTES';
+export const GET_PACIENTE_DETAILS = 'GET_PACIENTE_DETAILS';
+export const SAVE_PACIENTE = 'SAVE_PACIENTE';
+export const DELETE_PACIENTE = 'DELETE_PACIENTE';
+export const UPDATE_PACIENTE = 'UPDATE_PACIENTE';
 export const FETCH_ERROR = 'FETCH_ERROR';
 
 export function loadPacientes() {
@@ -10,8 +14,55 @@ export function loadPacientes() {
       method: 'GET',
     })
       .then(res => dispatch({
-        type: GET_PACIENTES, payload: res,
+        type: GET_PACIENTES, payload: res.filter(item => item.Status !== 99),
       }))
+      .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
+  };
+}
+
+export function addPaciente(data) {
+  return async (dispatch) => {
+    await APIResquest({
+      uri: 'pacientes',
+      method: 'POST',
+      data,
+    })
+      .then(res => dispatch({ type: SAVE_PACIENTE, payload: res }))
+      .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
+  };
+}
+
+export function loadPacienteDetail(id) {
+  return async (dispatch) => {
+    await APIResquest({
+      uri: `${id}/pacientes`,
+      method: 'GET',
+    })
+      .then(res => dispatch({ type: GET_PACIENTE_DETAILS, payload: res }))
+      .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
+  };
+}
+
+export function updatePaciente(data, id) {
+  return async (dispatch) => {
+    await APIResquest({
+      uri: `${id}/pacientes`,
+      method: 'PATCH',
+      data,
+    })
+      .then(res => dispatch({ type: UPDATE_PACIENTE, payload: res }))
+      .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
+  };
+}
+
+export function deletePaciente(data, id) {
+  return async (dispatch) => {
+    await APIResquest({
+      uri: `${id}/pacientes`,
+      method: 'PATCH',
+      data,
+    })
+      .then(() => dispatch({ type: DELETE_PACIENTE, payload: id }))
       .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
   };
 }
