78e855d98474543b4103436899d58daf708b2104
Bruno Macedo <contato@brunomacedo.com.br>
2019-03-22 20:49:53 -0300

    [feat] reducer patients

--- src/reducers/pacientes.js
+++ src/reducers/pacientes.js
@@ -1,24 +1,53 @@
 import {
-  GET_PACIENTES, FETCH_ERROR,
+  GET_PACIENTES, GET_PACIENTE_DETAILS,
+  SAVE_PACIENTE, DELETE_PACIENTE,
+  UPDATE_PACIENTE, FETCH_ERROR,
 } from '../actions/pacientes';
 
 const INITIAL_STATE = {
+  paciente: {},
   pacientes: [],
   fetchError: '',
 };
 
-export default function (state = INITIAL_STATE, action = {}) {
-  switch (action.type) {
+export default function (state = INITIAL_STATE, { type, payload }) {
+  switch (type) {
     case GET_PACIENTES:
       return {
         ...state,
-        pacientes: action.payload,
+        pacientes: payload,
+        fetchError: '',
+      };
+    case GET_PACIENTE_DETAILS:
+      return {
+        ...state,
+        paciente: payload,
+        fetchError: '',
+      };
+    case SAVE_PACIENTE:
+      return {
+        ...state,
+        paciente: payload,
+        fetchError: '',
+      };
+    case UPDATE_PACIENTE:
+      return {
+        ...state,
+        paciente: payload,
+        fetchError: '',
+      };
+    case DELETE_PACIENTE:
+      return {
+        ...state,
+        pacientes: state.pacientes.filter(item => (
+          item.PublicID !== payload
+        )),
         fetchError: '',
       };
     case FETCH_ERROR:
       return {
         ...state,
-        fetchError: action.payload,
+        fetchError: payload,
       };
     default:
       return state;
