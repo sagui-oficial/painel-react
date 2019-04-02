866918262f2523824071e32225e62a722dede617
Bruno Macedo <contato@brunomacedo.com.br>
2019-04-02 16:38:13 -0300

    [FEAT] add dashboard title

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Master from '../../components/Master';
 
-const Dashboard = () => (
-  <Master>
-    <div>Text here Dashboard</div>
+const Dashboard = ({ title }) => (
+  <Master title={title}>
+    <div>{title}</div>
   </Master>
 );
+
+Dashboard.propTypes = {
+  title: PropTypes.string,
+};
+
+Dashboard.defaultProps = {
+  title: String(),
+};
 
 export default Dashboard;
