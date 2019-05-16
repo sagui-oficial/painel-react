4a408cba6394cb8485410fb1fdaf563729eb4343
Bruno Macedo <contato@brunomacedo.com.br>
2019-05-16 10:51:09 -0300

    [FEAT] add fields

--- src/views/Guias/Form/index.jsx
+++ src/views/Guias/Form/index.jsx
@@ -10,11 +10,19 @@ import {
   Grid,
   TextField,
   Divider,
+  MenuItem,
 } from '@material-ui/core';
+
+import Select from 'react-select';
 
 import Message from '../../../components/Message';
 import Master from '../../../components/Master';
 import Breadcrumb from '../../../components/Breadcrumb';
+import { Control, Option } from '../../../components/AutoComplete';
+import { convertDatePicker, formatCurrency } from '../../../helpers';
+
+/* CONNECT */
+import { loadPacientes } from '../../../actions/pacientes';
 
 const styles = theme => ({
   divider: {
@@ -46,9 +54,37 @@ class GuiaForm extends Component {
     this.state = {
       isBlocking: false,
       editing: false,
+      selectedName: null,
       breadcrumb: [
         { label: 'Guias', url: '/guias' },
       ],
+      listStatus: [
+        { label: 'Criada', value: 1 },
+        { label: 'Concluída', value: 2 },
+      ],
+      valorTotal: 0,
+      AllGuias: [],
+      sendGuia: {
+        Status: 1,
+        Numero: String(),
+        Solicitacao: convertDatePicker(new Date()),
+        Vencimento: convertDatePicker(new Date()),
+        PlanoOperadora: {
+          id: Number(),
+          PublicID: String(),
+          NomeFantasia: String(),
+        },
+        Paciente: {
+          Nome: String(),
+          CPF: String(),
+          ListaPlanoOperadoraPaciente: Number(),
+        },
+      },
+      isValidField: {
+        Numero: false,
+        Nome: false,
+        PlanoOperadora: false,
+      },
       boxMessage: {
         open: false,
         text: '',
@@ -98,12 +134,15 @@ class GuiaForm extends Component {
   render() {
     const {
       classes, title,
-      match, error,
+      match, error, pacientes,
     } = this.props;
 
     const {
       breadcrumb, editing,
       boxMessage, isBlocking,
+      sendGuia, listStatus,
+      valorTotal, selectedName,
+      isValidField,
     } = this.state;
 
     return (
@@ -154,22 +193,184 @@ class GuiaForm extends Component {
 
         <form className={classes.form} noValidate autoComplete="off">
           <Grid container spacing={16}>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={3}>
               <TextField
                 fullWidth
                 required
                 label="Número da guia"
                 name="Numero"
-                // error={isValidField.Numero}
-                // value={sendPaciente.Numero}
-                // onChange={e => this.onHandleTarget(e.target)}
+                error={isValidField.Numero}
+                value={sendGuia.Numero}
+                onChange={e => this.onHandleTarget(e.target)}
                 // onBlur={e => this.onHandleBlur(e.target)}
                 helperText="Digite o número da guia"
                 margin="normal"
                 variant="outlined"
               />
             </Grid>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                fullWidth
+                select
+                value={sendGuia.Status}
+                onChange={e => this.onHandleTarget(e.target)}
+                label="Status"
+                name="Status"
+                margin="normal"
+                variant="outlined"
+              >
+                {listStatus.map(option => (
+                  <MenuItem key={option.value} value={option.value}>{option.label}</MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                fullWidth
+                value={formatCurrency(valorTotal)}
+                label="Valor total"
+                margin="normal"
+                variant="outlined"
+                InputProps={{
+                  readOnly: true,
+                }}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
           </Grid>
+
+          <Grid container spacing={16}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Data de solicitação"
+                name="Solicitacao"
+                type="date"
+                onChange={e => this.onHandleTarget(e.target)}
+                defaultValue={sendGuia.Solicitacao}
+                helperText="23/02/2019"
+                margin="normal"
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Data de vencimento"
+                name="Vencimento"
+                type="date"
+                onChange={e => this.onHandleTarget(e.target)}
+                defaultValue={sendGuia.Vencimento}
+                helperText="23/02/2019"
+                margin="normal"
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+          </Grid>
+
+          <br />
+
+          <Typography variant="h6" color="inherit" noWrap>
+            Informação sobre o paciente
+          </Typography>
+
+          <Grid container spacing={16}>
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              style={{
+                position: 'relative',
+                zIndex: '2',
+              }}
+            >
+              <Select
+                label="Nome do paciente"
+                options={pacientes.map(suggestion => ({
+                  id: suggestion.PublicID,
+                  value: suggestion.Nome,
+                  label: suggestion.Nome,
+                }))}
+                components={{ Control, Option }}
+                value={selectedName}
+                onChange={this.onHandleTargetPacienteNome}
+                placeholder="Selecione..."
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                disabled
+                label="CPF"
+                name="CPF"
+                value={sendGuia.Paciente.CPF}
+                onChange={this.onHandleTargetPaciente}
+                margin="normal"
+                variant="outlined"
+                helperText="020.000.009-92"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                fullWidth
+                disabled
+                label="Telefone"
+                name="Telefone"
+                margin="normal"
+                variant="outlined"
+                helperText="(11) 9000-0000"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                disabled
+                label="E-mail"
+                name="Email"
+                margin="normal"
+                variant="outlined"
+                helperText="email@email.com.br"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12}>
+              <TextField
+                fullWidth
+                disabled
+                label="Plano/Operadora"
+                name="NomeFantasia"
+                value={sendGuia.PlanoOperadora.NomeFantasia}
+                onChange={e => this.onHandleTarget(e.target)}
+                margin="normal"
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+          </Grid>
+
+          <br />
+
+          <Typography variant="h6" color="inherit" noWrap>
+            Adicionar procedimentos
+          </Typography>
 
           <Button
             type="submit"
@@ -192,6 +393,7 @@ class GuiaForm extends Component {
 GuiaForm.propTypes = {
   classes: PropTypes.instanceOf(Object).isRequired,
   match: PropTypes.instanceOf(Object).isRequired,
+  pacientes: PropTypes.instanceOf(Object).isRequired,
   error: PropTypes.string.isRequired,
   title: PropTypes.string,
 };
@@ -201,7 +403,10 @@ GuiaForm.defaultProps = {
 };
 
 const mapStateToProps = state => ({
+  pacientes: state.pacientesReducer.pacientes,
   error: state.guiasReducer.fetchError,
 });
 
-export default connect(mapStateToProps, {})(withStyles(styles)(GuiaForm));
+export default connect(mapStateToProps, {
+  loadPacientes,
+})(withStyles(styles)(GuiaForm));
