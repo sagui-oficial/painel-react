49ea21a68a4b8ad880ae405deb5cf9b9a51bf566
Bruno Macedo <contato@brunomacedo.com.br>
2019-05-25 18:12:56 -0300

    fix: unmount logout

--- src/components/Logout/index.jsx
+++ src/components/Logout/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import PropTypes from 'prop-types';
@@ -91,8 +90,6 @@ class MenuListComposition extends Component {
                     <CardActions>
                       <Button
                         onClick={propLogout}
-                        component={NavLink}
-                        to="/"
                         size="small"
                         color="default"
                       >
