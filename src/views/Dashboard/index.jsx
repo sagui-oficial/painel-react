3511dd9687fae9a9bca3c7ff96910eeb8d5d9c00
Bruno Macedo <contato@brunomacedo.com.br>
2019-07-02 22:04:35 -0300

    feat: update dashboard component with chart store

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
 import { Chart } from 'react-google-charts';
 import { Grid } from '@material-ui/core';
 
@@ -7,66 +9,82 @@ import {
   LibraryBooks as LibraryBooksIcon,
 } from '@material-ui/icons';
 
+import { loadData } from '../../actions/dashboard';
 import Master from '../../components/Master';
 import Loading from '../../components/Loading';
 import BoxChart from '../../components/BoxChart';
 
 const colors = ['#0098b8', '#f1c100'];
 
-const Dashboard = () => (
-  <Master>
-    <Grid container justify="space-between" spacing={16}>
-      <BoxChart
-        title="Faturamento mês"
-        data="R$ 28.900,90"
-        icon={DashboardIcon}
-        bgColor="#00b898"
-      />
-      <BoxChart
-        title="Guias glosadas"
-        data="R$ 900,90"
-        icon={LibraryBooksIcon}
-      />
-      <BoxChart
-        title="Planos cadastrados"
-        data="6"
-        icon={LibraryBooksIcon}
-      />
-    </Grid>
+class Dashboard extends Component {
+  componentDidMount() {
+    const { loadData: propsLoadData } = this.props;
+    propsLoadData();
+  }
 
-    <Grid container alignItems="center">
-      <Chart
-        width="100%"
-        height="400px"
-        chartType="Line"
-        loader={<Loading />}
-        style={{ marginTop: 10 }}
-        chartLanguage="pt-BR"
-        data={[
-          ['', 'Faturamento', 'Glosadas'],
-          ['01/2019', { v: 6600.90, f: 'R$ 6.600,90' }, { v: 350, f: 'R$ 350,00' }],
-          ['02/2019', 10450, 5280],
-          ['03/2019', 23400, 12420],
-          ['04/2019', 1776.8, 1180],
-          ['05/2019', 1860.78, 110],
-          ['06/2019', 9990.98, 890],
-          ['07/2019', 567.90, 89],
-          ['08/2019', 1200.60, 30],
-          ['09/2019', 3600.87, 856],
-          ['10/2019', 4500.24, 578],
-          ['11/2019', 8000.45, 450],
-          ['12/2019', 12000.79, 15680],
-        ]}
-        options={{
-          colors,
-          chart: {
-            title: 'Faturamento mensal de guias',
-            subtitle: 'valores em reais (R$)',
-          },
-        }}
-      />
-    </Grid>
-  </Master>
-);
+  render() {
+    const { planos } = this.props;
 
-export default Dashboard;
+    return (
+      <Master>
+        <Grid container justify="space-between" spacing={16}>
+          <BoxChart
+            title="Faturamento mês"
+            data="R$ 28.900,90"
+            icon={DashboardIcon}
+            bgColor="#00b898"
+          />
+          <BoxChart
+            title="Guias glosadas"
+            data="R$ 900,90"
+            icon={LibraryBooksIcon}
+          />
+          <BoxChart
+            title="Planos cadastrados"
+            data="6"
+            icon={LibraryBooksIcon}
+          />
+        </Grid>
+
+        <Grid container alignItems="center">
+          <Chart
+            width="100%"
+            height="400px"
+            chartType="Bar"
+            loader={<Loading />}
+            style={{ marginTop: 10 }}
+            chartLanguage="pt-BR"
+            data={planos && [
+              ['Planos', 'Faturamento', 'Glosadas'],
+              ...planos,
+            ]}
+            options={{
+              colors,
+              chartArea: { width: '100%' },
+              chart: {
+                title: 'Faturamento mensal de guias',
+                subtitle: 'valores em reais (R$)',
+              },
+              legend: {
+                position: 'bottom',
+              },
+            }}
+          />
+        </Grid>
+      </Master>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  planos: PropTypes.instanceOf(Object).isRequired,
+  loadData: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  planos: state.dashboardReducer.planos,
+});
+
+export default connect(mapStateToProps, {
+  loadData,
+})(Dashboard);
