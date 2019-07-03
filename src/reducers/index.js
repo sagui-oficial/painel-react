6d7522007c513b32582af8593c717b6777ef899e
Bruno Macedo <contato@brunomacedo.com.br>
2019-07-02 22:04:01 -0300

    feat: add chart reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
 import { firebaseReducer } from 'react-redux-firebase';
 
+import dashboardReducer from './dashboard';
 import lotesReducer from './lotes';
 import guiasReducer from './guias';
 import pacientesReducer from './pacientes';
@@ -9,6 +10,7 @@ import procedimentosReducer from './procedimentos';
 import planosReducer from './planos';
 
 export default combineReducers({
+  dashboardReducer,
   lotesReducer,
   guiasReducer,
   pacientesReducer,
