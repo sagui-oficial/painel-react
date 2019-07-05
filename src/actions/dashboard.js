43bdf0a2a243b57ec904d33c1a86551215aff228
Bruno Macedo <contato@brunomacedo.com.br>
2019-07-05 20:22:47 -0300

    feat: add dashboard endpoint

--- src/actions/dashboard.js
+++ src/actions/dashboard.js
@@ -1,49 +1,18 @@
+import { APIResquest } from '../utils/api';
+
 export const GET_DATA = 'GET_DATA';
+export const FETCH_ERROR = 'FETCH_ERROR';
 
 export function loadData() {
-  return {
-    type: GET_DATA,
-    payload: [
-      {
-        operadora: 'Dental Par',
-        total: 8260.90,
-        glosadas: 490.80,
-      },
-      {
-        operadora: 'Prodent',
-        total: 6900.67,
-        glosadas: 898.56,
-      },
-      {
-        operadora: 'Uniodonto',
-        total: 9104.55,
-        glosadas: 90.89,
-      },
-      {
-        operadora: 'Unimed',
-        total: 3409.77,
-        glosadas: 129.84,
-      },
-      {
-        operadora: 'Prevident',
-        total: 908.51,
-        glosadas: 10.70,
-      },
-      {
-        operadora: 'Interodonto',
-        total: 4560.09,
-        glosadas: 200.98,
-      },
-      {
-        operadora: 'SulAmérica',
-        total: 2090.88,
-        glosadas: 348.21,
-      },
-      {
-        operadora: 'Metlife',
-        total: 1130.90,
-        glosadas: 330.98,
-      },
-    ],
+  return async (dispatch) => {
+    await APIResquest({
+      uri: 'dashboard/2018-01-01/2019-12-12',
+      method: 'GET',
+    })
+      .then(res => dispatch({
+        type: GET_DATA,
+        payload: res.grafico || [],
+      }))
+      .catch(err => dispatch({ type: FETCH_ERROR, payload: err }));
   };
 }
