608722531715c0e7875601144c308e3c857df2f2
Bruno Macedo <contato@brunomacedo.com.br>
2019-07-09 16:21:18 -0300

    feat: add items objects

--- src/reducers/dashboard.js
+++ src/reducers/dashboard.js
@@ -2,7 +2,10 @@ import { GET_DATA } from '../actions/dashboard';
 import { formatCurrency } from '../helpers';
 
 const INITIAL_STATE = {
-  planos: [],
+  grafico: [],
+  faturamento: {},
+  guiasGlosadas: {},
+  pacienteAtendidos: 0,
 };
 
 export default function (state = INITIAL_STATE, { type, payload }) {
@@ -10,13 +13,16 @@ export default function (state = INITIAL_STATE, { type, payload }) {
     case GET_DATA:
       return {
         ...state,
-        planos: payload.map(item => (
+        grafico: payload.grafico && payload.grafico.map(item => (
           [
             item.operadora,
             { v: item.total, f: formatCurrency(item.total) },
             { v: item.glosadas, f: formatCurrency(item.glosadas) },
           ]
         )),
+        guiasGlosadas: payload.guiasGlosadas && payload.guiasGlosadas,
+        faturamento: payload.faturamento && payload.faturamento,
+        pacienteAtendidos: payload.pacienteAtendidos && payload.pacienteAtendidos,
       };
     default:
       return state;
