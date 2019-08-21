f27e12489aabd7080abd3bb058f9e5dfc301db50
Bruno Macedo <contato@brunomacedo.com.br>
2019-08-21 19:38:33 -0300

    fix: required plan on save patient

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -46,6 +46,6 @@ export const validateCPF = (strCPF) => {
 };
 
 export const validateEmail = (strEmail) => {
-  const emailFormat = /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  const emailFormat = /\S+@\S+\.\S+/;
   return emailFormat.test(strEmail);
 };
